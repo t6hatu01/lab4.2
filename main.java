8bdd784978d5dda399e79f3f425c92b1a9908d32
Tuuli <t6hatu01@students.oamk.fi>
2017-09-27 16:10:08 +0300

    koodi2

--- main.java
+++ main.java
@@ -1,8 +1,9 @@
 public class main {
     public static void main(String[] args) { 
-    	Product product = new Product("Edam", 3.3, 120);
+    	Product product = new Product("Edam", 3.3, 120, 150);
     	System.out.println("Product value is " + product.countValue());
-    	product.printProduct();
+    	System.out.println("New value is " + product.changeBalance());
+        product.printProduct();
      }
 }
 
@@ -10,14 +11,21 @@ public class main {
 	private String name;
 	private double price;
 	private int amount; //Amount in storage
-	public Product(String name, double price, int amount) {
+	private int newvalue;
+
+	public Product(String name, double price, int amount, int newvalue) {
 		this.name = name;
 		this.price = price;
 		this.amount = amount;
+        this.newvalue = newvalue;
 	}
 	public double countValue() {
 		return(amount * price);
 	}
+
+	public double changeBalance(){
+        return(newvalue * price);
+    }
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
